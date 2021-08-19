014f12e5cfa5484a8379748583acc67a8a7876ba
Trace <tracedelange@me.com>
2021-08-19 09:50:09 -0500

    commit tree 86599ba628cbad3adafbca17cb8f003c5f0917be

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 12, "M", "Brown", 'Kind', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 12, "M", "Brown", 'Kind', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 12, "M", "Brown", 'Kind', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 12, "M", "Brown", 'Kind', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 12, "M", "Brown", 'Kind', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 12, "M", "Brown", 'Kind', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 12, "M", "Brown", 'Kind', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (null, 12, "M", "Brown", 'Kind', true);
